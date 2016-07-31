183b2af547c5ce3a628f514e297cac2a701f17db
Jay Stevens <jay@mandarincreativegroup.com>
2016-07-31 11:39:59 -0400

    Initial Commit of source for XKCD password generator

---
+++ xkcdpasswd/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace xkcdpasswd
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string arg1 = "4";  // default # of words
+            if (args.Length > 0)
+                arg1 = args[0];            
+
+            string sep = "-";
+            int numWords = int.Parse(arg1);
+            int i = 0;
+            bool alt = true;
+            StringBuilder sb = new StringBuilder();
+            Random rnd = new Random();
+            if (rnd.Next(0, 1) == 1)
+                alt = true;
+
+            do
+            {
+                if (i > 0)
+                    sb.Append(sep);
+
+                var n = rnd.Next(0, Dictionary.EN.Length);
+                var wrd = Dictionary.EN[n];
+
+                if (alt)
+                    wrd = wrd.ToUpper();
+
+                sb.Append(wrd);
+                i++;
+                alt = !alt;
+            } while (i < numWords);
+
+            Console.WriteLine(sb.ToString());
+        }        
+    }
+}
---
+++ xkcdpasswd/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mandarin Creative Group")]
+[assembly: AssemblyProduct("xkcdpasswd")]
+[assembly: AssemblyTrademark("2016 by Mandarin Creative Group")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0f5c1eac-f54c-4968-a1e6-ba43f291b800")]
